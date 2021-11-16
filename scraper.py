c11a1ef9da51023c0d23f6f3374fd3a64722fff3
Zhukov Oleksandr <oleksandrzhukov@gmail.com>
2021-11-16 23:08:42 +0200

    Created: "collect_movie_html" function
    The function collects movie.html files

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 import requests
 from bs4 import BeautifulSoup
@@ -29,9 +30,25 @@ def collect_href(path_to_file):
         json.dump(all_films, file, indent=4, ensure_ascii=False)
 
 
+def collect_movie_html(path_to_file, html_collection):
+    with open(path_to_file, "r") as file:
+        all_films_list = json.load(file)
+        os.makedirs(html_collection)
+    counter = 1
+    for film_name, film_href in all_films_list.items():
+        request_ = requests.get(film_href)
+        response = request_.text
+
+        with open(f"{html_collection}/{film_name}.html", "w") as file:
+            file.write(response)
+        print(f"HTML for {film_name} was created [{counter}/{len(all_films_list)}]")
+        counter += 1
+
+
 def main():
     # get_html(url, f"{data_dir}/tomato.html")
-    collect_href(f"{data_dir}/tomato.html")
+    # collect_href(f"{data_dir}/tomato.html")
+    collect_movie_html(f"{data_dir}/all_films_list.json", f"{data_dir}/html_collect")
 
 
 if __name__ == "__main__":
