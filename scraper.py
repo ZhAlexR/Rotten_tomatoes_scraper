2a9602d47fb5a377db51e324cac87d91aafcd77b
Zhukov Oleksandr <oleksandrzhukov@gmail.com>
2021-11-16 21:13:00 +0200

    Created "get_html" method
    The "get_html" method takes url from "settings.py" and creates "tomatoes.html"
    in the directory wich path was added to "data_dir" in "settings.py"

--- scraper.py
+++ scraper.py
@@ -1,8 +1,18 @@
 import requests
 
+from settings import url, data_dir
+
 
 def get_html(set_url, save_to):
     site_url = set_url
     response = requests.get(site_url)
     with open(save_to, "w") as file:
         file.write(response.text)
+
+
+def main():
+    get_html(url, f"{data_dir}/tomato.html")
+
+
+if __name__ == "__main__":
+    main()
