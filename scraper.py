a68f67a4a157cd89593f9f19bd1997f6405a5419
Zhukov Oleksandr <oleksandrzhukov@gmail.com>
2021-11-16 21:47:56 +0200

    Created: "collect_href" function
    The function takes html file ("tomatoes.html")
    and collects "film_name" and "single_page_href" to "all_films_list.json"

--- scraper.py
+++ scraper.py
@@ -1,4 +1,7 @@
+import json
+
 import requests
+from bs4 import BeautifulSoup
 
 from settings import url, data_dir
 
@@ -10,8 +13,25 @@ def get_html(set_url, save_to):
         file.write(response.text)
 
 
+def collect_href(path_to_file):
+    with open(path_to_file, "r") as source:
+        html_doc = source.read()
+    soup = BeautifulSoup(html_doc, "lxml")
+    films_href = soup.find(class_="table").find_all("a", class_="unstyled articleLink")
+
+    all_films = {}
+    for item in films_href:
+        film_name = item.text.strip()
+        single_page_href = f"https://www.rottentomatoes.com{item.get('href')}"
+        all_films[film_name] = single_page_href
+
+    with open(f"{data_dir}/all_films_list.json", "w") as file:
+        json.dump(all_films, file, indent=4, ensure_ascii=False)
+
+
 def main():
-    get_html(url, f"{data_dir}/tomato.html")
+    # get_html(url, f"{data_dir}/tomato.html")
+    collect_href(f"{data_dir}/tomato.html")
 
 
 if __name__ == "__main__":
