9a71838712b419684704524a6baa9812b45c0cc2
Zhukov Oleksandr <oleksandrzhukov@gmail.com>
2021-11-17 00:11:31 +0200

    Created: "collect_data" and "write_data" functions

--- scraper.py
+++ scraper.py
@@ -1,10 +1,12 @@
+import csv
 import json
 import os
+import re
 
 import requests
 from bs4 import BeautifulSoup
 
-from settings import url, data_dir
+from settings import url, data_dir, headers
 
 
 def get_html(set_url, save_to):
@@ -45,10 +47,39 @@ def collect_movie_html(path_to_file, html_collection):
         counter += 1
 
 
+def collect_data(path_to_folder):
+    data_list = []
+    for item in os.listdir(path_to_folder):
+        with open(f"{path_to_folder}/{item}", "r") as file:
+            single_film_html = file.read()
+        soup = BeautifulSoup(single_film_html, "lxml")
+        movie_name = soup.find("h1", class_="scoreboard__title").text
+        movie_info = soup.find("ul", class_="content-meta info").find_all("li")
+        data = {"Film": movie_name, }
+
+        for info in movie_info:
+            div_item = info.find_all("div")
+            clean_label = re.sub(r'^\s+|:|\s+$', '', div_item[0].text)
+            if clean_label in headers:
+                value = re.sub(r'^\s+|\n|\s+(?= \w)|\s+$', '', div_item[1].text)
+                data[clean_label] = value
+        data_list.append(data)
+    return data_list
+
+
+def write_data(file_name, data):
+    with open(file_name, "w") as file:
+        dict_writer = csv.DictWriter(file, fieldnames=headers)
+        dict_writer.writeheader()
+        dict_writer.writerows(data)
+
+
 def main():
     # get_html(url, f"{data_dir}/tomato.html")
     # collect_href(f"{data_dir}/tomato.html")
-    collect_movie_html(f"{data_dir}/all_films_list.json", f"{data_dir}/html_collect")
+    # collect_movie_html(f"{data_dir}/all_films_list.json", f"{data_dir}/html_collect")
+    data = collect_data(f"{data_dir}/html_collect")
+    write_data(f"{data_dir}/movie_info.csv", data)
 
 
 if __name__ == "__main__":
