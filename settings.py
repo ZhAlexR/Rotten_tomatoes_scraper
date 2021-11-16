8237b493d14d34298d70a4b92d192279030f4186
Zhukov Oleksandr <oleksandrzhukov@gmail.com>
2021-11-17 00:09:15 +0200

    Added: headers

--- settings.py
+++ settings.py
@@ -1,2 +1,7 @@
 url = 'https://www.rottentomatoes.com/top/bestofrt/'
+
 data_dir = "/home/zhukov_oleksandr/programming/python/parsing/rotten_tomatoes_scraper/data"
+
+headers = ["Film", "Genre", "Original Language", "Director",
+           "Producer", "Writer", "Release Date (Streaming)",
+           "Runtime", "Sound Mix", "Aspect Ratio"]
